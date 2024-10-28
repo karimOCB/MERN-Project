098b28ccf8896f7ec3f5e97776166bebed789125
Chaya <chaya.karim.omar@gmail.com>
2024-10-27 22:05:29 -0300

    0.2 Mongo Connection and Env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./utils/validateEnv";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-  res.send("Hello Worldciro!");
-});
+const port = env.PORT;
 
-app.listen(port, () => {
-console.log(`Server running on port https://localhost:${port}`);
-});
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(console.error);
