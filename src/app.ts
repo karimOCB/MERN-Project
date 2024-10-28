81d1cab79b822d61bc39fc26d2d7cf0f882ac492
Chaya <chaya.karim.omar@gmail.com>
2024-10-28 17:40:34 -0300

    0.3 Model, Router, Controller & HTTP

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Worldciro!");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
